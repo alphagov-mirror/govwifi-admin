82b6b704a4020eb6acb34942ab7bb486c3fe6e4a
Robin Mitra <robin.mitra@digital.cabinet-office.gov.uk>
2019-10-29 15:59:18 +0000

    wip

--- spec/features/mega_admin/sign_in_as_super_admin_spec.rb
+++ spec/features/mega_admin/sign_in_as_super_admin_spec.rb
@@ -4,6 +4,7 @@ describe 'Signing in as super admin', type: :feature, focus: true do
   context 'when visiting the home page' do
     it 'shows super admin panel' do
       visit root_path
+      sign_in_user user
 
       expect(page).to have_content("New shiny super admin")
     end
